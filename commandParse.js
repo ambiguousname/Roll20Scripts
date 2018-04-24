79250a9b570ba73230af0d00b71bff078b82e5ff
Tyly04 <tyly04@gmail.com>
2018-04-24 01:46:23 +0000

    Made Script changes

--- commandParse.js
+++ commandParse.js
@@ -1,28 +1,29 @@
 //Simple thing for parsing commands
 //As of now, arguments have to be separated by one special character. There can't be multiple special characters.
+var parseString;
 on("ready", function(){
-    state.commandParse = {
-        parseString: function(string, specialChar){
-            var args = [];
-            var argString = "";
-            for(var i in string){
-                var char = string[i];
-                var append = "";
-                if(char === "\'" || char === "\""){
-                    //Hope this works. Might have to change it.
-                    append = "\\";
-                }
-                if(char === specialChar){
-                    args.push(argString);
-                    argString = "";
-                } else if (i === string.length - 1){
-                    argString += append + char;
-                    args.push(argString);
-                } else {
-                    argString += append + char;
-                }
+    parseString = function(string, specialChar){
+        var args = [];
+        var argString = "";
+        for(var i in string){
+            var char = string[i];
+            log(string.length);
+            var append = "";
+            if(char === "\'" || char === "\""){
+                //Hope this works. Might have to change it.
+                append = "\\";
             }
-            return args;
+            if(char === specialChar){
+                args.push(argString);
+                argString = "";
+            } else if (parseInt(i) === string.length - 1){
+                log(i);
+                argString += append + char;
+                args.push(argString);
+            } else {
+                argString += append + char;
+            }
         }
-    };
+        return args;
+    }
 });
--- patrol.js
+++ patrol.js
@@ -6,47 +6,62 @@
 //arg1 = The name (or name(s)) of the tokens to patrol.
 //arg2 = Coordinate pairs. Stylized: x,y:x2,y2:x3,y3
 on("ready", function(){
+    sendChat("Patrol.js", "Patrol.js is online!");
     if(!state.tPatrol){
         state.tPatrol = {
             guards: []
         };
+    } else {
+        state.tPatrol.guards.forEach(function(guard){
+            clearInterval(guard.int);
+        });
     }
 });
 function setPatrol(coordArr, guard, ms){
+    sendChat("Patrol.js", "Patrol set.");
+    sendChat("Patrol.js", "Guard Type: " + guard.get("_type"));
+    sendChat("Patrol.js", "Guard Name: " + guard.get("name"));
+    sendChat("Patrol.js", "Guard is patrolling: " + guard.patrol);
     if(guard.patrol === true){
         var index = 0;
-        var prevPos = state.command.parseString(coordArr[0], ",");
+        var prevPos = parseString(coordArr[0], ",");
         var countUp = true;
-        guard.int = window.setInterval(function(){
-            if(countUp){
+        guard.int = setInterval(function(){
+            //Increment and decrement index
+            var coord = parseString(coordArr[index], ",");
+            var dist = [parseInt(coord[0]) - parseInt(prevPos[0]),parseInt(coord[1]) - parseInt(prevPos[1])];
+            guard.set("left", guard.get("left") + dist[0]);
+            guard.set("top", guard.get("top") + dist[1]);
+            prevPos = coord;
+            if(countUp === true){
                 index += 1;
-                if(index >= coordArr.length){
-                    index = coordArr.length - 1;
+                if(index > coordArr.length - 2){
                     countUp = false;
                 }
             } else {
                 index -= 1;
-                if(index < 0){
-                    index = 0;
+                if(index < 1){
                     countUp = true;
                 }
             }
-            var coord = state.commandParse.parseString(coordArr[index], ",");
-            var dist = [Math.abs(parseInt(coord[0]) - parseInt(prevPos[0])),Math.abs(parseInt(coord[1]) - parseInt(prevPos[1]))];
-            guard.set("left", guard.get("left") + dist);
-            prevPos = coord;
         }, ms);
+        guard.patrol = false;
     } else {
         window.clearInterval(guard.int);
     }
 }
 on("chat:message", function(msg){
     if(msg.type === "api" && msg.content.indexOf("!pt ") !== -1){
-        var args = state.commandParse.parseString(msg.content, " ");
+        sendChat("Patrol.js", "Analyzing Patrol Pattern...");
+        var args = parseString(msg.content, " ");
         var guards = findObjs({name: args[1]});
-        guards.forEach(function(g){
+        sendChat("Patrol.js", "Found " + guards.length + " guard(s) with name " + args[1] + ".");
+        _.each(guards, function(g){
+            sendChat("Patrol.js", "Guard " + g.get("name") + " found.");
             g.patrol = true;
-            setPatrol(state.commandParse.parseString(args[2], ":"), g, 1000);
+            log(args);
+            setPatrol(parseString(args[2], ":"), g, 1000);
+            sendChat("Patrol.js", "Setting Parameters...");
             state.tPatrol.guards.push(g);
         });
     }
