46903c40363ce875263a9a3c048201043b6c1f28
Tyly04 <tyly04@gmail.com>
2018-04-24 02:17:51 +0000

    Removed some stuff

--- commandParse.js
+++ commandParse.js
@@ -1,7 +1,6 @@
 //Simple thing for parsing commands
 //As of now, arguments have to be separated by one special character. There can't be multiple special characters.
 var parseString;
-var parseQString;
 on("ready", function(){
     parseString = function(string, specialChar){
         var args = [];
@@ -26,27 +25,5 @@ on("ready", function(){
             }
         }
         return args;
-    }
-    //Too lazy to rename stuff.
-    parseQString = function(string, specialChar){
-        var fullStr = "";
-        var nString = "";
-        for(var i in string){
-            var char = string[i];
-            var append = "";
-            if(char === "\'" || char === "\""){
-                //Hope this works. Might have to change it.
-                append = "\\";
-            }
-            if(char === specialChar){
-                fullStr += argString;
-                argString = "";
-            } else if (parseInt(i) === string.length - 1){
-                argString += append + char;
-                fullStr += argString;
-            } else {
-                argString += append + char;
-            }
-        }
     }
 });
